d915d0b2979b55ed9cbbf4b26442311d1f55493e
STIEBELJOSHUA <stiebeljoshua@gmail.com>
2022-12-04 21:14:35 -0500

    more primitaves

--- klack.c
+++ klack.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <sys/param.h>
 #include "klack.h"
 
 //macros
@@ -13,6 +13,17 @@
 static Token* stackhead = NULL;
 
 //helper functions
+
+//pop top value from stack
+Token* popped(){
+  Token* pop = stackhead;
+  if(pop==NULL){
+    printf("STACK IS EMTPY\n");
+    return NULL;
+  }
+  stackhead = pop->prev;
+  return pop;
+}
 
 //append to double array
 void darrayappend(Token* tk, double dub){
@@ -26,7 +37,7 @@ void darrayappend(Token* tk, double dub){
 }
 
 
-void printarray(Token* tk){
+void printarray(Token* tk, int ischar){
   int prints = tk->len/tk->rows;
   if(tk->matrix.d==NULL){
     printf("()\n");
@@ -37,57 +48,207 @@ void printarray(Token* tk){
     if((i)==prints)
       printf("\n ");
     double db = tk->matrix.d[i];
-    if((int)db==db)
+    if(ischar)
+      printf("%c",(char)tk->matrix.d[i]);
+    else if((int)db==db)
       printf("%d",(int)tk->matrix.d[i]);
     else
       printf("%.4f",tk->matrix.d[i]);
-    if(i+1!=tk->len)
+    if(i+1!=tk->len && !ischar)
       printf(" ");
   }
   printf(")\n");
   
 }
 
-//primitive implementations
-
-void add(){
-  printf("add test\n");
-}
-
-void sub(){
-  printf("subtract test\n");
-}
-
-void printstack(){
+void printstack(int ischar){
   Token* headcopy = stackhead;
   if(headcopy!=NULL){
     while(headcopy->prev!=NULL){
-      printarray(headcopy);
+      printarray(headcopy,ischar);
       headcopy = headcopy->prev;
     }
-    printarray(headcopy);
+    printarray(headcopy,ischar);
   }
 }
 
+//TODO: this is slightly broken, addition and all others too return diffrent depending on order
+// ie (1 2 3)(2)+ returns (3 4 5), while (2)(1 2 3)+ returns (3). 
+void dubop(char ch){
+  if(stackhead==NULL || stackhead->prev==NULL){
+    puts("NOT ENOUGH ELEMENTS ON STACK");
+    return;
+  }
+  Token* tk1 = popped();
+  Token* tk2 = stackhead;
+  if(tk1->len==1){
+    for(int i=0;i<tk2->len;i++){
+      double r;
+      switch(ch){
+      case '+' :
+         r = tk2->matrix.d[i]+tk1->matrix.d[0];
+         break;
+      case '-' :
+         r = tk2->matrix.d[i]-tk1->matrix.d[0];
+         break;
+      case '/' :
+          r = tk2->matrix.d[i]/tk1->matrix.d[0];
+          break;
+      case '*' :
+          r = tk2->matrix.d[i]*tk1->matrix.d[0];
+          break;
+      case '%' :
+          r = (int)tk2->matrix.d[i]%(int)tk1->matrix.d[0];
+          break;
+      case '>' :
+          r = tk2->matrix.d[i]>tk1->matrix.d[0]?1:0;
+          break;
+      case '<' :
+          r = tk2->matrix.d[i]<tk1->matrix.d[0]?1:0;
+          break;
+      case '=' :
+          r = tk2->matrix.d[i]==tk1->matrix.d[0];
+          break;
+      }
+      tk2->matrix.d[i]=r;
+    }
+  }
+  else if(tk1->len>1){
+    int iter = MIN(tk1->len,tk2->len);
+    for(int i=0;i<iter;i++){
+      double r;
+      switch(ch){
+        case '+' :
+          r = tk2->matrix.d[i]+tk1->matrix.d[i];
+          break;
+        case '-' :
+          r = tk2->matrix.d[i]-tk1->matrix.d[i];
+        case '/' :
+          r = tk2->matrix.d[i]/tk1->matrix.d[i];
+          break;
+        case '*' :
+          r = tk2->matrix.d[i]*tk1->matrix.d[i];
+          break;
+        case '%' :
+          r = (int)tk2->matrix.d[i]%(int)tk1->matrix.d[i];
+          break;
+        case '>' :
+          r = tk2->matrix.d[i]>tk1->matrix.d[i]?1:0;
+          break;
+        case '<' :
+          r = tk2->matrix.d[i]<tk1->matrix.d[i]?1:0;
+          break;
+        case '=' :
+          r = tk2->matrix.d[i]==tk1->matrix.d[i]?1:0;
+          break;
+        
+      }
+      tk2->matrix.d[i]=r;
+    }
+  }
+  else{
+    puts("ERROR, 0 length array");
+  }
+  free(tk1->matrix.d);
+  free(tk1);
+}
+
+//primitive implementations
+
+void add(){
+  dubop('+');
+}
+
+void sub(){
+  dubop('-');
+}
+
+void divd(){
+  dubop('/');
+}
+
+void mult(){
+  dubop('*');
+}
+
+void mod(){
+  dubop('%');
+}
+
+void gt(){
+  dubop('>');
+}
+
+void lt(){
+  dubop('<');
+}
+
+void eq(){
+  dubop('=');
+}
+
+void sum(){
+  double* arr = stackhead->matrix.d;
+  double sumd = 0;
+  for(int i=0;i<stackhead->len;i++){
+    sumd+=arr[i];
+  }
+  
+  stackhead->matrix.d=malloc(sizeof(double)*10);
+  stackhead->size=10;
+  stackhead->len=0;
+  darrayappend(stackhead,sumd);
+  
+  free(arr);
+}
+
+
+void pop(){
+  Token* tk = popped();
+  if(tk==NULL)
+    return;
+  free(tk->matrix.d);
+  free(tk);
+}
+
+
+void dprintstack(){
+  printstack(0);
+}
+
+void sprintstack(){
+  printstack(1);
+}
+
 //primitave "dict"
-char prims[] = "+-p";
+char prims[] = "+-pP./*%><=s";
 void (*prim_func[128])();
 
 void init_prims(){
   prim_func[(int)'+']=add;
   prim_func[(int)'-']=sub;
-  prim_func[(int)'p']=printstack;
+  prim_func[(int)'p']=dprintstack;
+  prim_func[(int)'P']=sprintstack;
+  prim_func[(int)'.']=pop;
+  prim_func[(int)'/']=divd;
+  prim_func[(int)'*']=mult;
+  prim_func[(int)'%']=mod;
+  prim_func[(int)'>']=gt;
+  prim_func[(int)'<']=lt;
+  prim_func[(int)'=']=eq;
+  prim_func[(int)'s']=sum;
 }
 
 //defined function "dict"
 char* def_funcs[128];
 //defined variables "dict"
-char* def_vars[128];
+Token* def_vars[128];
 //execute a chunk of code MAKE SURE ITS NULL TERMINATED
 void execline(char* line){
   int i = strlen(line);
   
   for(int j=0;j<i;j++){
+    int oldjtmp=j;
     char ch = line[j];
     
     //ignore spaces and newlines
@@ -101,8 +262,19 @@ void execline(char* line){
     }
     //check for defined variables
     else if(ch=='~'){
+
       ch=line[++j];
-      execline(def_vars[(int)ch]);
+      
+      Token* var = def_vars[(int)ch];
+      Token* tk = malloc(sizeof(Token));
+      *tk=*var;
+      tk->matrix.d=malloc(sizeof(double)*tk->size);
+      memcpy(tk->matrix.d,var->matrix.d,sizeof(double)*tk->size);
+      
+      tk->prev=stackhead;
+      stackhead = tk;
+      
+      
     }
     //check for primitaves
     else if(strchr(prims,ch)){
@@ -179,9 +351,9 @@ void execline(char* line){
           ch=line[++j];
           char name = ch;
           ch=line[++j];
-          while(ch!=':'){
+          while(ch!=';'){
             if(ch=='\0'){
-              fprintf(stderr,"missing ending :\n");
+              fprintf(stderr,"missing ending ;\n");
               return;
             }
             ch=line[++j];
@@ -195,31 +367,26 @@ void execline(char* line){
         }
      
     //define variable 
-    else if(ch==';'){
-          int start=j;
-          ch=line[++j];
-          char name = ch;
-          ch=line[++j];
-          while(ch!=';'){
-            if(ch=='\0'){
-              fprintf(stderr,"missing ending ;\n");
-              return;
-            }
-            ch=line[++j];
-          }
-          int len = j-start-2;
-          char* stri = malloc(len+1);
-          stri[len]='\0';
-          strncpy(stri,&line[start+2],len);
-          free(def_vars[(int)name]);
-          def_vars[(int)name]=stri;
-        }
+    else if(ch=='$'){
+      free(def_vars[(int)ch]);
+      
+      ch=line[++j];
+      Token* tk = malloc(sizeof(Token));
+      *tk=*stackhead;
+
+      tk->matrix.d=malloc(sizeof(double)*tk->size);
+      memcpy(tk->matrix.d,stackhead->matrix.d,sizeof(double)*tk->size);
+
+      def_vars[(int)ch]=tk;
+      }
     
     else{
       fprintf(stderr, "%c is not a valid character!\n",ch);
       return;
     }
-  }  
+  
+    int oldj = oldjtmp;
+  }
 }
 
 
